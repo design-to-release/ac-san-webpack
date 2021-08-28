9f0d9ac18898c2605259cf9910ad5b13b7dba687
木杉 <zhmushan@qq.com>
2021-08-28 22:49:00 +0800

    Fix parse error & add tests (#2)
    * Fix parse error & add tests
    * fix script parse error

--- dist/plugin/utility-types.d.ts
+++
@@ -1,7 +0,0 @@
-/** PartialLiteral */
-export declare type PL = 'Partial';
-/** RequiredLiteral */
-export declare type RL = 'Required';
-export declare type PartialOrRequired<T, F extends PL | RL> = {
-    [P in keyof T]: F extends PL ? Partial<T[P]> : Required<T[P]>;
-};
